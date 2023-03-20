20e69672b1ed84d1b79e1860d9fd9d8a1648b672
newhask777 <newhaskellisp@gmail.com>
2023-03-20 20:42:07 +0300

    redis

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,17 +1,26 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
-import json
-import datetime
+import time
 
+from fastapi_cache.decorator import cache
 from src.database import get_async_session
 from src.operations.models import operation
 from src.operations.schemas import OperationCreate
+
 
 router = APIRouter(
     prefix="/operations",
     tags=["Operation"]
 )
+
+
+@router.get("/long_operation")
+@cache(expire=30)
+def get_long_op():
+    time.sleep(2)
+    return "Много много данных, которые вычислялись сто лет"
+
 
 
 @router.get("/")
