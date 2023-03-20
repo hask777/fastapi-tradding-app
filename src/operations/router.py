aa75cdfe69d50aa8976ca57818adf47b2819aeb6
newhask777 <newhaskellisp@gmail.com>
2023-03-20 20:11:03 +0300

    errors

--- src/operations/router.py
+++ src/operations/router.py
@@ -25,19 +25,9 @@ async def get_specific_operations(operation_type: str, session: AsyncSession = D
 @router.post("/")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
     stmt = insert(operation).values(**new_operation.dict())
-    # for op in new_operation.dict():
-    #     print(op)
-    # print(new_operation.id)
-    # stmt = insert(operation).values(id=2233, quantity='fg', figi='45', instrument_type='fg', date=datetime.datetime.now(), type='dfd')
-    # print(stmt)
 
     await session.execute(stmt)
     await session.commit()
     return {"status": "success"}
 
-"""
-2023-03-18T13:42:28.123Z
-
-2023-03-18T14:02:35.224Z
-"""
 
